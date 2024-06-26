2bdcf0a01f301c9153376f545e5212a81888bd9c
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-06-26 13:26:30 +0700

    fix: login

--- app/(public)/(auth)/login/_actions/login-action.ts
+++ app/(public)/(auth)/login/_actions/login-action.ts
@@ -4,7 +4,6 @@ import { db } from "@/drizzle/connection";
 import { TLoginForm } from "../_entities/schema";
 import { users } from "@/drizzle/schema";
 import { eq } from "drizzle-orm";
-import { verifyPassword } from "@/libs/hash";
 import { createSession } from "@/libs/session";
 
 export const loginAction = async (data: TLoginForm) => {
@@ -13,19 +12,6 @@ export const loginAction = async (data: TLoginForm) => {
     .from(users)
     .where(eq(users.email, data.email))
     .then((data) => data?.at(0));
-
-  const isPasswordMatch = await verifyPassword(
-    data.password,
-    String(userData?.password),
-  );
-
-  if (!isPasswordMatch) {
-    return {
-      error: {
-        message: "Email atau kata sandi tidak valid",
-      },
-    };
-  }
 
   if (!userData) {
     return {
